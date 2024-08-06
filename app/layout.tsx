0c451adaf46db136a0a6eeb6cdde256d9b9935d1
sachinjadhav <sachinjdv7@gmail.com>
2024-08-06 12:06:53 +0530

    setup radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import "./globals.css";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
